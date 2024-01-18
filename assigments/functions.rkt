1f6687f847d32242626b433fc90d5ff4f70b21d1
deparr <dparrott@protonmail.com>
2024-01-18 15:15:43 -0700

    clean up lists and do functions

--- assigments/functions.rkt
+++ assigments/functions.rkt
@@ -10,9 +10,20 @@
          #t
          temps))
 
-(define (convert digits) ....)
+(define (convert digits)
+  (fst (foldl (lambda (d acc)
+                (let ([sum (fst acc)]
+                      [mag (snd acc)])
+                  (pair (+ sum (* mag d)) (* mag 10))))
+              (pair 0 1)
+              digits)))
 
-(define (average-price nums) ....)
+(define (average-price nums)
+  (let ([sum (foldr + 0 nums)]
+        [count (length nums)])
+    (if (zero? count)
+        (error 'badaverage "bad div")
+        (/ sum count))))
 
 (define (convertFC fs)
   (map (lambda (temp)
@@ -26,11 +37,23 @@
   (lambda (val)
     (after (before val))))
 
-(define (flatten loloa) ....)
+(define (flatten loloa)
+  (type-case (Listof (Listof 'a)) loloa
+    [empty (list)]
+    [(cons loa r) (append loa (flatten r))]))
 
-(define (flatten-foldr) ....)
+(define (flatten-foldr loloa)
+  (foldr append (list) loloa))
 
-(define (bucket lon) ....)
+(define (bucket lon)
+  (foldr (lambda (n acc)
+           (type-case (Listof 'a) acc
+             [empty (cons (cons n empty) empty)]
+             [(cons cur-bkt rst-bkts)
+              (if (= n (first cur-bkt))
+                  (cons (cons n cur-bkt) rst-bkts)
+                  (cons (cons n empty) acc))]))
+         (list) lon))
 
 (define-type Pedigree
   (person [name : String]
@@ -40,6 +63,35 @@
           [mother : Pedigree])
   (unknown))
 
-(define (tree-map f pedigree) ....)
+(define (tree-map proc pedigree)
+  (type-case Pedigree pedigree
+    [(unknown) (unknown)]
+    [(person n b e f m)
+     (person (proc n) b e (tree-map proc f) (tree-map proc m))]))
 
-(define (add-last-name pedigree last-name) ....)
+(define (add-last-name pedigree last-name)
+  (let ([last-name (string-append " " last-name)])
+    (tree-map (lambda (n)
+                (string-append n last-name))
+              pedigree)))
+
+(test (convert (list 1 2 3)) 321)
+
+(test ((compose-func add1 (lambda (v) (* v 2))) 5) 11)
+(test ((compose-func (lambda (v) (* v 2)) add1) 5) 12)
+
+(test (flatten (list (list 1 2) (list 3 4 5) (list 6))) (list 1 2 3 4 5 6))
+(test (flatten empty) empty)
+(test (flatten-foldr (list (list 1 2) (list 3 4 5) (list 6))) (list 1 2 3 4 5 6))
+(test (flatten-foldr empty) empty)
+
+(test (bucket '(1 1 2 2 2 3 1 1 1 2 3 3 ))
+      (list (list 1 1) (list 2 2 2 ) (list 3) (list 1 1 1) (list 2) (list 3 3)))
+(test (bucket empty) (list))
+
+(test (add-last-name (person "child" 1997 'gray
+                             (person "father" 1970 'brown (unknown) (unknown)) (person "mother" 1971 'blue (unknown) (unknown)))
+                     "last-name")
+      (person "child last-name" 1997 'gray
+              (person "father last-name" 1970 'brown (unknown) (unknown))
+              (person "mother last-name" 1971 'blue (unknown) (unknown))))
--- assigments/lists.rkt
+++ assigments/lists.rkt
@@ -2,72 +2,54 @@
 (print-only-errors #t)
 
 (define (find-temperate temps)
-  (cond
-    [(empty? temps) (none)]
-    [(cons? temps) (let ([val (first temps)])
-                     (if (and (<= val 95) (>= val 5))
-                         (some val)
-                         (find-temperate (rest temps))))]))
+  (type-case (Listof 'a) temps
+    [empty (none)]
+    [(cons t restts) (if (and (<= t 95) (>= t 5))
+                         (some t)
+                         (find-temperate restts))]))
 
 (define (check-temperate temps)
   (check-temps temps 5 95))
 
 (define (check-temps temps low high)
-  (letrec ([check-bounds
-            (lambda (vals)
-              (cond
-                [(empty? vals) #t]
-                [(cons? vals)
-                 (let ([val (first vals)])
-                   (if (or (> val high) (< val low))
-                       #f
-                       (check-bounds (rest vals))))]))])
-    (check-bounds temps)))
+  (type-case (Listof Number) temps
+    [empty #t]
+    [(cons t restts)
+     (and
+      (<= t high)
+      (>= t low)
+      (check-temps restts low high))]))
+
+(define (number-from-digits digits mag)
+  (type-case (Listof Number) digits
+    [empty 0]
+    [(cons d restds)
+     (+ (* d mag)
+        (number-from-digits restds (* 10 mag)))]))
 
 (define (convert digits)
-  (letrec ([walk (lambda (digits mag)
-                   (cond
-                     [(empty? digits) 0]
-                     [(cons? digits)
-                      (+ (* (first digits) mag)
-                         (walk (rest digits) (* 10 mag)))]))])
-    (walk digits 1)))
+  (number-from-digits digits 1))
+
+(define (average nums sum count)
+  (type-case (Listof Number) nums
+    [empty (/ sum count)]
+    [(cons n restns) (average
+                      restns
+                      (+ sum n)
+                      (add1 count))]))
 
 (define (average-price prices)
-  (letrec ([average
-            (lambda (nums sum count)
-              (cond
-                [(empty? nums) (/ sum count)]
-                [(cons? nums) (average
-                               (rest nums)
-                               (+ sum (first nums))
-                               (add1 count))]))])
-    (if (cons? prices)
-        (average prices 0 0)
-        0)))
+  (if (cons? prices)
+      (average prices 0 0)
+      (error 'badaverage "bad div")))
 
-; dont think we're allowed to use map here
-#;(define (convertFC fahrenheits)
-    (map (lambda (temp)
-           (* (- temp 32) 5/9))
-         fahrenheits))
+(define (convertFC fahrenheits)
+  (map (lambda (temp)
+         (* (- temp 32) 5/9))
+       fahrenheits))
 
-(define (convertFC fs)
-  (cond
-    [(empty? fs) empty]
-    [(cons? fs)
-     (cons (* (- (first fs) 32) 5/9) (convertFC (rest fs)))]))
-
-
-; jank filter implementation
 (define (eliminate-exp ua lop)
-  (letrec ([filter (lambda (vals proc)
-                     (cond
-                       [(empty? vals) (list)]
-                       [(cons? vals) (if (not (proc (first vals)))
-                                         (filter (rest vals) proc)
-                                         (cons (first vals) (filter (rest vals) proc)))]))])
-    (filter lop (lambda (val) (<= val ua)))))
+  (filter (lambda (val) (<= val ua)) lop))
 
 (define (suffixes l)
   (cond
@@ -88,13 +70,17 @@
      (+ 1 (+ (count-persons f) (count-persons m)))]
     [(unknown) 0]))
 
+(define (sum-ages pedigree)
+  (type-case Pedigree pedigree
+    [(person _n b _e f m)
+     (+ (+ (- 2023 b) (sum-ages f)) (sum-ages m))]
+    [(unknown) 0]))
+
 (define (average-age pedigree)
-  (letrec ([sum-ages (lambda (pedigree)
-                       (type-case Pedigree pedigree
-                         [(person _n b _e f m)
-                          (+ (+ (- 2023 b) (sum-ages f)) (sum-ages m))]
-                         [(unknown) 0]))])
-    (/ (sum-ages pedigree) (count-persons pedigree))))
+  (let ([count (count-persons pedigree)])
+    (if (zero? count)
+        (error 'badaverage "bad div")
+        (/ (sum-ages pedigree) count))))
 
 (define (eye-colors pedigree)
   (type-case Pedigree pedigree
@@ -113,6 +99,7 @@
 (test (check-temps '(0 10 20 30 40 50 60 70 80 90 100) 0 100) #t)
 (test (check-temps '(-10) 0 100) #f)
 (test (check-temps '(200) 0 100) #f)
+(test (check-temps '() 0 100) #t)
 
 (test (convert '(1 2 3)) 321)
 (test (convert '(5 4 3 2 1)) 12345)
@@ -120,7 +107,7 @@
 
 (test (average-price '(5 5 5 5 5 5)) 5)
 (test (average-price '(1 2 3 4 5 6 7)) 4)
-(test (average-price '()) 0)
+(test/exn (average-price '()) "bad div")
 
 (test (convertFC '(32 212 -40)) '(0 100 -40))
 
