18ad0adcdaecd15c5d17c26e6b510ae5c8d48589
deparr <dparrott@protonmail.com>
2024-04-11 15:15:14 -0600

    almost done with lazy

--- assignments/lazy.rkt
+++ assignments/lazy.rkt
@@ -1,20 +1,6 @@
 #lang plait #:lazy
 ;; David Parrott - dmparr22
 
-(define (f10 l)
-  (begin
-  (display (list-ref l 0))
-  (display (list-ref l 1))
-  (display (list-ref l 2))
-  (display (list-ref l 3))
-  (display (list-ref l 4))
-  (display (list-ref l 5))
-  (display (list-ref l 6))
-  (display (list-ref l 7))
-  (display (list-ref l 8))
-  (display (list-ref l 9))))
-
-
 ;; (take-while pred xs) → (Listof 'a)
 ;;   pred : ('a -> Boolean)
 ;;   xs : (Listof 'a)
@@ -23,39 +9,70 @@
     [empty
      empty]
     [(cons x rest-xs)
-		(if (pred x)
-		  (cons x (take-while pred rest-xs))
-          empty)]))
+     (if (pred x)
+         (cons x (take-while pred rest-xs))
+         empty)]))
 
+(define (seq start)
+  (cons start (seq (add1 start))))
+
+(define (take-n l n)
+  (type-case (Listof 'a) l
+    [empty
+     empty]
+	[(cons v rls)
+	 (if (> n 0)
+	 (cons v (take-n rls (- n 1)))
+	 empty)]))
 
 ;; (build-infinite-list f) → (Listof 'a)
 ;;	f : (Number -> 'a)
 (define (build-infinite-list f)
-  (cons (f 0) (build-infinite-list f)))
+  (map f (seq 0)))
 
 ;; nats : (Listof Number)
 (define nats
-  (build-infinite-list add1))
+  (build-infinite-list (lambda (n) n)))
 
-(f10 nats)
+(define (all pred l)
+  (type-case (Listof 'a) l
+    [empty
+     #t]
+    [(cons v rls)
+     (and (pred v) (all pred rls))]))
 
 ;; (prime? n) → Boolean
 ;;	n : Number
+#;
 (define (prime? n)
-  ....)
+  (all (lambda (d) (> (remainder n d) 0))
+       (take-while (lambda (d) (< (* d d) n))
+                   (rest (rest nats)))))
+(define (prime? n)
+  (foldl (lambda (d a) (and a (> (remainder n d) 0))) #t
+         (take-while (lambda (d) (< (* d d) n))
+                     (rest (rest nats)))))
 
 ;; primes : (Listof Number)
-(define primes 
-  ....)
+(define primes
+  (filter prime? (rest (rest nats))))
+
+(list-ref primes 1000)
 
 ;; (prime?/fast n) → Boolean
 ;;	n : Number
 (define (prime?/fast n)
-  ....)
+  (foldl (lambda (d a) (and a (> (remainder n d) 0))) #t
+         (take-n primes/fast n)))
+#;
+(define (prime?/fast n)
+  (foldl (lambda (d a) (and a (> (remainder n d) 0))) #t
+         (take-while (lambda (d) (< (* 2 d) n))
+                     primes/fast)))
 
 ;; primes/fast : (Listof Number)
-(define primes/fast 
-  ....)
+(define primes/fast
+  (filter prime?/fast (rest (rest nats))))
 
+(list-ref primes/fast 0)
 
-;;
